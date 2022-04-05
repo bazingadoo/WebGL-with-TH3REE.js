becbcc1e3743c595cf1767bf3f611605ac2d3afe
rawwr <akshayace0302@gmail.com>
2022-04-05 20:11:23 +0530

    fixes full screen for safari

--- 05_Cameras/src/script.js
+++ 05_Cameras/src/script.js
@@ -2,14 +2,6 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// console.log(OrbitControls)
-
-// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
-
-// Add the event listeners for cursor(vanilla JS)
-
-//cursor variables
-
 const cursor = {
   x: 0,
   y: 0,
@@ -24,9 +16,6 @@ window.addEventListener("mousemove", (e) => {
   // console.log(cursor.y)
 });
 
-/**
- * Base
- */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -54,10 +43,20 @@ window.addEventListener("resize", () => {
 
 //adding fullscreen mode by double clicking anywhere
 window.addEventListener("dblclick", () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen();
+  const fullScreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullScreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
   } else {
-    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
   }
 });
 
@@ -79,26 +78,13 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
-// //Orthographic camera
-// const aspectRatio = sizes.width / sizes.height
-// console.log(aspectRatio)
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 1000)
-
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 3;
 
-//length of the camera
-// console.log(camera.position.length())
 camera.lookAt(mesh.position);
 scene.add(camera);
 
-//the damping will smooth the animation by adding acceleration
-//and friction
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// controls.target.y = 1
-// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -113,16 +99,6 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  // Update objects
-  // mesh.rotation.y = elapsedTime
-
-  //update camera to move the cam around the center of the scene
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  // camera.position.y = cursor.y * 5   //for up and down of the cube
-  // camera.lookAt(mesh.position)
-
-  //Update controls(updating Damping)
   controls.update();
   // Render
   renderer.render(scene, camera);
